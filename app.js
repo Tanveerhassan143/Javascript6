dcdb540a39b6b3cd3578d409db6640a01894d5b5
unknown <ranatanveerhassan04@gmail.com>
2023-11-20 21:22:09 +0500

    aas

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var b = prompt("enter the length of table")
 for (var n = 1; n <= a; n++) {
     for (var n = 1; n <= b; n++) {
         for (var c = a * n; c <= a * n; c++) {
-            document.write(a + "x" + n + "<br>"  + "=" + c+ "<br>")
+            document.write(a + "x" + n + "<br>" + "=" + c + "<br>")
         }
     }
 
@@ -24,11 +24,11 @@ for (var n = 1; n <= a; n++) {
 
 
 
-// Revecing counting
+// Revercing counting
 
 document.write("<br>")
 document.write("<hr>")
-document.write("<h1>Revcing Counting</h1>")
+document.write("<h1>Revercing Counting</h1>")
 
 var a = prompt("Enter the numbr for Revercing Counting")
 for (var n = 15; n >= a; n--) {
@@ -40,10 +40,27 @@ document.write("<br>")
 document.write("<hr>")
 document.write("<h1>Even number</h1>")
 
-var a = 17
-for (var n =2; n <= a; n+=2) {
-    document.write(n + "<br>");
+
+for (var a = 1; a <= 20; a++) {
+    if (a % 2 == 0) {
+        document.write(a + "<br>");
+
+    }
 }
+
+
+
+document.write("<br>")
+document.write("<hr>")
+document.write("<h1>Even number</h1>")
+
+
+for (var a = 1; a <= 20; a++) {
+    if (a % 2 != 0) {
+        document.write(a + "<br>");
+    }
+}
+
 
 
 
@@ -52,9 +69,10 @@ document.write("<hr>")
 document.write("<h1>series number</h1>")
 
 var a = 20
-for (var n =2; n <= a; n+=2) {
-    document.write(n+"K" +"," );
+for (var n = 2; n <= a; n += 2) {
+    document.write(n + "K" + ",");
 }
+
 
 
 
